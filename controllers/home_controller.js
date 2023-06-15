52012f76ecf980c728a3459ee2a0c7846ba094b1
Rahul <rahulsharma2rk2329@gmail.com>
2023-06-15 12:55:34 +0530

    display post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,18 @@
-module.exports.home=function(req,res){
+const Post = require("../models/post");
+module.exports.home = function (req, res) {
+  //   Post.find({}, function (err, posts) {
+  //     return res.render("home", {
+  //       title: "Codeial | Home",
+  //       posts: posts,
+  //     });
+  //   });
 
-    return res.render('home',{
-        title:"Home"
+  Post.find({})
+    .populate("user")
+    .exec(function (err, posts) {
+      return res.render("home", {
+        title: "Codeial | Home",
+        posts: posts,
+      });
     });
-}
+};
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
-
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-
 //console.log(passport)
-const homeController=require('../controllers/home_controller')
+const homeController = require("../controllers/home_controller");
 console.log("Router loaded");
-router.get('/',homeController.home);
+router.get("/", homeController.home);
 
-router.use('/users',require('./users'));
-module.exports=router;
+router.use("/users", require("./users"));
 
+router.use("/posts", require("./posts"));
+
+module.exports = router;
